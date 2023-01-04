b6315db5f256e50e2456f82d279204fc69edc00a
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-01-04 19:30:25 +0530

    chapter 2: lesson 1: string concatenation

---
+++ chapter2/lesson1/string_concatenation.js
@@ -0,0 +1,21 @@
+const student = {
+  name: 'Richard Kalehoff',
+  guardian: 'Mr. Kalehoff'
+};
+
+const teacher = {
+  name: 'Mrs. Wilson',
+  room: 'N231'
+};
+
+let message = student.name + ' please see ' + teacher.name + ' in ' + teacher.room + ' to pick up your report card.';
+
+console.log(message);
+
+let note = teacher.name + ',\n\n' +
+  'Please excuse ' + student.name + '.\n' +
+  'He is recovering from the flu.\n\n' +
+  'Thank you,\n' +
+  student.guardian;
+
+console.log(note);
