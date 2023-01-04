23dac5200a35aa156301bf6f52c7cbd8abe0b0ff
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-01-04 19:12:32 +0530

    chapter 2: lesson 1: quiz 2 const

---
+++ chapter2/lesson1/quiz2_const.js
@@ -0,0 +1,10 @@
+function getClothing(isCold) {
+  if(isCold) {
+    const freezing = 'Grab a jacket';
+  } else {
+    const hot = 'It’s a shorts kind of day.';
+    console.log(freezing);
+  }
+}
+
+getClothing(false);
