62c1a68378855dd265b172b19932429ad17d7a76
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-01-06 18:40:55 +0530

    chapter 2: lesson 1: object literal, and it's shorthand

---
+++ chapter2/lesson1/object_literal.js
@@ -0,0 +1,11 @@
+let type = 'quartz';
+let color = 'rose';
+let carat = 21.29;
+
+const gemstone = {
+  type: type,
+  color: color,
+  carat: carat
+};
+
+console.log(gemstone);
---
+++ chapter2/lesson1/object_literal_shorthand.js
@@ -0,0 +1,7 @@
+let type = 'quartz';
+let color = 'rose';
+let carat = 21.29;
+
+const gemstone = { type, color, carat };
+
+console.log(gemstone);
