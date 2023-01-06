a0b90e2ba4dc733c63f5adaea5420891fbfd61e8
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-01-06 18:32:45 +0530

    chapter 2: lesson 1: destructuring values from array and object

---
+++ chapter2/lesson1/destructuring_values_from_array.js
@@ -0,0 +1,9 @@
+const point = [10, 25, -34];
+
+const [x, y, z] = point;
+
+console.log(x, y, z);
+
+const [m, , n] = point;
+
+console.log(m ,n);
---
+++ chapter2/lesson1/destructuring_values_from_object.js
@@ -0,0 +1,9 @@
+const gemstone = {
+  type: 'quartz',
+  color: 'rose',
+  carat: 21.29
+};
+
+const { type, color, carat } = gemstone;
+
+console.log(type, color, carat);
