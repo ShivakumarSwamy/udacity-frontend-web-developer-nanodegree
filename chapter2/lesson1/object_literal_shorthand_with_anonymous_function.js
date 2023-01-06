7b693bb5044a8280aa5fcea0663749852d009aa0
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-01-06 18:45:50 +0530

    chapter 2: lesson 1: object literal shorthand with anonymous function and method name

---
+++ chapter2/lesson1/object_literal_shorthand_with_anonymous_function.js
@@ -0,0 +1,14 @@
+let type = 'quartz';
+let color = 'rose';
+let carat = 21.29;
+
+const gemstone = {
+  type,
+  color,
+  carat,
+  calculateWorth: function() {
+    return 10;
+  }
+};
+
+console.log(gemstone.calculateWorth());
