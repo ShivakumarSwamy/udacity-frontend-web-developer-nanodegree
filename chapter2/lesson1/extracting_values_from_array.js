2e6643a95f30b5b68cbe458c83be75e9e170b1f6
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-01-06 18:23:07 +0530

    chapter 2: lesson 1: extracting values from array and object

---
+++ chapter2/lesson1/extracting_values_from_array.js
@@ -0,0 +1,7 @@
+const point = [10, 25, -34];
+
+const x = point[0];
+const y = point[1];
+const z = point[2];
+
+console.log(x, y, z);
---
+++ chapter2/lesson1/extracting_values_from_object.js
@@ -0,0 +1,11 @@
+const gemstone = {
+  type: 'quartz',
+  color: 'rose',
+  carat: 21.29
+};
+
+const type = gemstone.type;
+const color = gemstone.color;
+const carat = gemstone.carat;
+
+console.log(type, color, carat);
