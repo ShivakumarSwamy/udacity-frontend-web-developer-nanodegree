8be500550d9e36728c874752669bae8de6ed2b0b
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-01-06 20:05:40 +0530

    chapter 2: lesson 1: spread operator

---
+++ chapter2/lesson1/spread_operator.js
@@ -0,0 +1,5 @@
+const books = ["Don Quixote", "The Hobbit", "Alice in Wonderland", "Tale of Two Cities"];
+console.log(...books);
+
+const primes = new Set([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+console.log(...primes);
