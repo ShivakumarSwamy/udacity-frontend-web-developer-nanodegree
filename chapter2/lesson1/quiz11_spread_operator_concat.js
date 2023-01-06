b4e8d87326116ddf5a3e594423c92e5588834924
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-01-06 20:05:49 +0530

    chapter 2: lesson 1: quiz 11 spread operator concat

---
+++ chapter2/lesson1/quiz11_spread_operator_concat.js
@@ -0,0 +1,10 @@
+/*
+ * Instructions: Use the spread operator to combine the `fruits` and `vegetables` arrays into the `produce` array.
+ */
+
+const fruits = ["apples", "bananas", "pears"];
+const vegetables = ["corn", "potatoes", "carrots"];
+
+const produce = [...fruits, ...vegetables];
+
+console.log(produce);
