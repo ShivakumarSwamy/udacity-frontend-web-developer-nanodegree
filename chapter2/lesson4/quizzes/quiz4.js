565354ff2e3a0f476067344e2dbf435e74c19f85
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-01-23 19:45:01 +0530

    chapter 2: lesson 4: javascript object equals

---
+++ chapter2/lesson4/quizzes/quiz4.js
@@ -0,0 +1,8 @@
+const a = {
+    myFunction: function quiz() { console.log('hi'); }
+};
+const b = {
+    myFunction: function quiz() { console.log('hi'); }
+};
+
+console.log(a.myFunction === b.myFunction)
---
+++ chapter2/lesson4/quizzes/quiz5.js
@@ -0,0 +1,10 @@
+function quiz() {  }
+
+const a = {
+    myFunction: quiz
+};
+const b = {
+    myFunction: quiz
+}
+
+console.log(a.myFunction === b.myFunction)
