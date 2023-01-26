88dad1346164be681327c73ff2bd2a5e770768f1
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-01-26 19:14:07 +0530

    chapter 2: lesson 4: quiz event phases

---
+++ chapter2/lesson4/quizzes/quiz7.js
@@ -0,0 +1,7 @@
+document.addEventListener('click', function () {
+   console.log('The document was clicked');
+});
+
+document.querySelector('h1').addEventListener('click', function () {
+    console.log('The document h1 was clicked');
+});
