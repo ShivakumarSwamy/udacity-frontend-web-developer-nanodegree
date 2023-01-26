fc12e6c6756be43ea6be503d7399b7dc4564df06
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-01-26 18:57:29 +0530

    chapter 2: lesson 4: remove event listener different object

---
+++ chapter2/lesson4/remove_event_listener_different_object.js
@@ -0,0 +1,7 @@
+document.addEventListener('click', function myEventListeningFunction() {
+                                       console.log('howdy');
+                                   });
+
+document.removeEventListener('click', function myEventListeningFunction() {
+                                       console.log('howdy');
+                                   });
