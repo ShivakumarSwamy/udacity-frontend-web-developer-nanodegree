3a5673b825bb045b2444731288c3117027ee08c9
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-02-04 22:38:22 +0530

    chapter 3: lesson 2: local server

---
+++ chapter3/lesson2/local_server/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const cors = require("cors")
+app.use(cors())
+
+const port = 8000
+const server = app.listen(port, listening)
+
+function listening() {
+  console.log("server running")
+  console.log(`running on localhost: ${port}`)
+}
