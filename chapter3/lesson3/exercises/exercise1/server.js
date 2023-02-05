00e467823c2c50a634cb9f4bd35ea07a86a45cd9
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2023-02-05 18:48:23 +0530

    chapter 3: lesson 3: exercise get request

---
+++ chapter3/lesson3/exercises/exercise1/server.js
@@ -0,0 +1,33 @@
+/* Empty JS object to act as endpoint for all routes */
+projectData = {};
+
+/* Express to run server and routes */
+const express = require('express');
+
+/* Start up an instance of app */
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser')
+/* Middleware*/
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
+
+/* Initialize the main project folder*/
+app.use(express.static('website'));
+
+const port = 3000;
+/* Spin up the server*/
+const server = app.listen(port, listening);
+ function listening(){
+    // console.log(server);
+    console.log(`running on localhost: ${port}`);
+  };
+
+// ROUTES!
+app.get("/all", function(request, response) {
+    response.send(projectData)
+})
+
